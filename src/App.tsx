1b4ee6eb32e844c2c05ff00e16f0046e86642d59
kevinbatard <kevin.batard4064@gmail.com>
2023-03-30 16:37:09 +0200

    reglage de la carte au centre + couleurs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './carte-style.css';
+import Carte from './carte/components/Carte';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className=""></header>
+            <main>
+                <div className="d-flex justify-content-center align-items-center mt-4">
+                    <Carte />
+                </div>
+            </main>
+            <footer></footer>
+        </div>
+    );
 }
 
 export default App;
