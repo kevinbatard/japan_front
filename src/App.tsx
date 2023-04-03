50894c7d016673c457dcc1e10864ddc92bda774c
kevinbatard <kevin.batard4064@gmail.com>
2023-04-03 09:26:58 +0200

    feat card apparaissent au survol d'une region, ajout des images pour chaque region et du logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import './carte-style.css';
+import Card from './carte/components/Card';
 import Carte from './carte/components/Carte';
 
 function App() {
+    const [showComponent, setShowComponent] = useState(false);
     return (
         <div className="App">
-            <header className=""></header>
             <main>
-                <div className="d-flex justify-content-center align-items-center mt-4">
-                    <Carte />
+                <div className="d-flex mt-4 justify-content-around container">
+                    <div className="me-5 d-flex flex-column">
+                        <img
+                            className="logo ps-4 mb-5"
+                            src="./img/logo.png"
+                            alt="logo Japan's Travels"
+                        />
+                        <div className="mt-5">{showComponent && <Card />}</div>
+                    </div>
+                    <div className="ms-5 carte">
+                        <Carte setShowComponent={setShowComponent} />
+                    </div>
                 </div>
             </main>
             <footer></footer>
