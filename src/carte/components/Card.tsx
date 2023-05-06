31e3001cb63c2d9e48d18706bff514d88847c05c
kevinbatard <kevin.batard4064@gmail.com>
2023-05-06 12:37:27 +0200

    opti code useless

--- src/carte/components/Card.tsx
+++ src/carte/components/Card.tsx
@@ -1,25 +1,25 @@
-import { TRegion } from '../../Types/TRegion';
-import './styles/card-style.css';
+import { TRegion } from "../../Types/TRegion";
+import "./styles/card-style.css";
 
 export default function Card(props: {
-    regionHover: string;
-    region: TRegion[] | undefined;
+  regionHover: string;
+  region: TRegion[] | undefined;
 }) {
-    const dataRegion = props.region
-        ?.map((elm) => elm)
-        .filter((elm) => elm.name === props.regionHover);
+  const dataRegion = props.region?.filter(
+    (elm) => elm.name === props.regionHover
+  );
 
-    return (
-        <div className="card m-auto card-style">
-            <img
-                src={`./img/${props.regionHover}.jpg`}
-                className="card-img-top img-size"
-                alt={` ${props.regionHover}`}
-            />
-            <div className="card-body">
-                <h1>{dataRegion![0].name}</h1>
-                <p className="card-text">{dataRegion![0].description_mini}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="card m-auto card-style">
+      <img
+        src={`./img/${props.regionHover}.jpg`}
+        className="card-img-top img-size"
+        alt={` ${props.regionHover}`}
+      />
+      <div className="card-body">
+        <h1>{dataRegion![0].name}</h1>
+        <p className="card-text">{dataRegion![0].description_mini}</p>
+      </div>
+    </div>
+  );
 }
