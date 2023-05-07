2e7d309baf5846351d4bbd09bafb5207f1fa8e3d
kevinbatard <kevin.batard4064@gmail.com>
2023-05-07 10:27:47 +0200

    ajout link entre connexion register

--- src/navbar/components/ConnexionModal.tsx
+++ src/navbar/components/ConnexionModal.tsx
@@ -1,142 +1,152 @@
-import { useContext, useState } from 'react';
-import './styles/modal-style.css';
-import { toast } from 'react-toastify';
-import { BASE_URL } from '../../constant/URL';
-import Cookies from 'js-cookie';
-import { ConnectedContext } from '../../context/user-context';
+import { useContext, useState } from "react";
+import "./styles/modal-style.css";
+import { toast } from "react-toastify";
+import { BASE_URL } from "../../constant/URL";
+import Cookies from "js-cookie";
+import { ConnectedContext } from "../../context/user-context";
 
 export default function ConnexionModal() {
-    const [ident, setIdent] = useState<string>('');
-    const [mdp, setMdp] = useState<string>('');
-    const { onUserChange, onTokenChange } = useContext(ConnectedContext);
+  const [ident, setIdent] = useState<string>("");
+  const [mdp, setMdp] = useState<string>("");
+  const { onUserChange, onTokenChange } = useContext(ConnectedContext);
 
-    const notifySuccess = (msg: string) =>
-        toast.success(msg, {
-            position: 'bottom-right',
-            autoClose: 1500,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: false,
-            draggable: true,
-            progress: undefined,
-            theme: 'light',
-        });
+  const notifySuccess = (msg: string) =>
+    toast.success(msg, {
+      position: "bottom-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
 
-    const notifyError = (msg: string) =>
-        toast.error(msg, {
-            position: 'bottom-right',
-            autoClose: 3000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: false,
-            draggable: true,
-            progress: undefined,
-            theme: 'light',
-        });
+  const notifyError = (msg: string) =>
+    toast.error(msg, {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
 
-    const connexion = () => {
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ pseudo: ident, password: mdp }),
-        };
-
-        fetch(`${BASE_URL}/auth/login`, options)
-            .then((response) => response.json())
-            .then((response) => {
-                if (response.data) {
-                    Cookies.set('token', response.data.access_token, {
-                        secure: false,
-                        expires: 7,
-                    });
-                    Cookies.set('user', JSON.stringify(response.data.user), {
-                        secure: false,
-                        expires: 7,
-                    });
-                    onUserChange(response.data.user);
-                    onTokenChange(response.data.access_token);
-                    notifySuccess(response.message);
-                } else {
-                    notifyError(response.message);
-                }
-            })
-            .catch((err) => console.error(err));
+  const connexion = () => {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ pseudo: ident, password: mdp }),
     };
 
-    return (
-        <>
-            <div
-                className="modal fade"
-                id="connexion"
-                tabIndex={-1}
-                aria-labelledby="connexion"
-                aria-hidden="true"
-            >
-                <div className="modal-dialog modal-dialog-centered ">
-                    <div className="modal-content  fw-bold">
-                        <div className="modal-header">
-                            <h1
-                                className="modal-title fs-4 text-style fw-bold modal-style"
-                                id="exampleModalLabel"
-                            >
-                                Connexion
-                            </h1>
-                            <button
-                                type="button"
-                                className="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                            ></button>
-                        </div>
+    fetch(`${BASE_URL}/auth/login`, options)
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.data) {
+          Cookies.set("token", response.data.access_token, {
+            secure: false,
+            expires: 7,
+          });
+          Cookies.set("user", JSON.stringify(response.data.user), {
+            secure: false,
+            expires: 7,
+          });
+          onUserChange(response.data.user);
+          onTokenChange(response.data.access_token);
+          notifySuccess(response.message);
+        } else {
+          notifyError(response.message);
+        }
+      })
+      .catch((err) => console.error(err));
+  };
 
-                        <div className="modal-body">
-                            <div className="mb-3">
-                                <label
-                                    htmlFor="exampleInputEmail1"
-                                    className="form-label modal-style"
-                                >
-                                    Identifiant :
-                                </label>
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    id="exampleInputEmail1"
-                                    aria-describedby="emailHelp"
-                                    onChange={(e) => setIdent(e.target.value)}
-                                ></input>
-                                <div className="my-3">
-                                    <label
-                                        htmlFor="exampleInputPassword1"
-                                        className="form-label modal-style"
-                                    >
-                                        Mot de passe :
-                                    </label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="exampleInputPassword1"
-                                        onChange={(e) => {
-                                            setMdp(e.target.value);
-                                        }}
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="modal-footer d-flex justify-content-center">
-                            <button
-                                type="button"
-                                className="btn btn-primary"
-                                onClick={connexion}
-                                data-bs-dismiss="modal"
-                                id="loginButton"
-                            >
-                                Connexion
-                            </button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div
+        className="modal fade"
+        id="connexion"
+        tabIndex={-1}
+        aria-labelledby="connexion"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered ">
+          <div className="modal-content  fw-bold">
+            <div className="modal-header">
+              <h1
+                className="modal-title fs-4 text-style fw-bold modal-style"
+                id="exampleModalLabel"
+              >
+                Connexion
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
-        </>
-    );
+
+            <div className="modal-body">
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleInputEmail1"
+                  className="form-label modal-style"
+                >
+                  Identifiant :
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  onChange={(e) => setIdent(e.target.value)}
+                ></input>
+                <div className="my-3">
+                  <label
+                    htmlFor="exampleInputPassword1"
+                    className="form-label modal-style"
+                  >
+                    Mot de passe :
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    onChange={(e) => {
+                      setMdp(e.target.value);
+                    }}
+                  ></input>
+                </div>
+                <div className="fw-light">
+                  Pas encore de compte ?{" "}
+                  <a
+                    data-bs-toggle="modal"
+                    data-bs-target="#register"
+                    href="/#"
+                  >
+                    Cliquez-ici !
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="modal-footer d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={connexion}
+                data-bs-dismiss="modal"
+                id="loginButton"
+              >
+                Connexion
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
